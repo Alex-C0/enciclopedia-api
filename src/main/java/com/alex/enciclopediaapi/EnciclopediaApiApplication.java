9e54824cb3cc45045e542de5eb85722cb7a952a9
Alejandro Cortes <aregularmail@gmail.com>
2026-03-12 19:36:02 -0600

    Proyecto inicial: enciclopedia-api generado con Spring Initializr

---
+++ src/main/java/com/alex/enciclopediaapi/EnciclopediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.alex.enciclopediaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnciclopediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnciclopediaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/enciclopediaapi/EnciclopediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.enciclopediaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnciclopediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
